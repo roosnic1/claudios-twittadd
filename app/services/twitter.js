cdc8957a03b5369a29637c6d08a89bc4c57a6559
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-08-30 17:45:58 +0200

    twitter service uses oauth

--- app/services/twitter.js
+++ app/services/twitter.js
@@ -2,7 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
 
-	autehnticate: function() {
+	init: function() {
+		this._super();
+		OAuth.initialize('fvf8IDzcTXgJgb5S3hNzKMchYvY');
+	},
+
+	authenticate: function() {
+		OAuth.popup('twitter').done(function(twitter) {
+			console.log(twitter);
+		  //make API calls with `twitter`
+		}).fail(function(err) {
+		  //todo when the OAuth flow failed
+		});
 
 	}
 });
