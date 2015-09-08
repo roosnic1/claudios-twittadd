cf55b9cbbdcec266bae2f5a1d098da9a1963ac2d
Nicolas Roos <nicolas.roos@serranetga.com>
2015-09-08 16:25:14 +0200

    added some structure and functionality

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 	twitter: Ember.inject.service(),
 
+	twittaccounts: '',
+
 	init: function() {
 		this._super();
 		console.log('Application Controller init');
@@ -13,9 +15,16 @@ export default Ember.Controller.extend({
 		testTwitter: function() {
 			this.get('twitter').authenticate();
 		},
+
+
+
 		initService: function() {
-			console.log('initS');
-			//this.get('twitter').startInit();
+			/*var id = this.get('twitter.user.id');
+			console.log('Getting friends of Id: ' + id);
+			this.get('twitter.result').get('https://api.twitter.com/1.1/friends/ids.json?cursor=-1&user_id='+id+'&count=5000').done(function(data) {
+				console.log(data);
+			});*/
+			console.log(this.get('twittaccounts'));
 		}
 	}
 });
