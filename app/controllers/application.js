bcb16719d3e4b1dfe9d363af0b0c0a0a33ba3a06
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-09-12 17:54:44 +0200

    implemented get twitter account infos

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,11 +4,30 @@ export default Ember.Controller.extend({
 
 	twitter: Ember.inject.service(),
 
-	twittaccounts: '',
+	twittAccounts: '',
+	twittAccountInfos: [],
 
 	init: function() {
 		this._super();
 		console.log('Application Controller init');
+	},
+
+
+	getAccountInfos: function(accounts) {
+		var screenNames = '';
+		accounts.forEach(function(item,index) {
+			screenNames += item;
+			if(index + 1 < accounts.length) {
+				screenNames += ',';
+			}
+		});
+		var self = this;
+		this.get('twitter.result').get('https://api.twitter.com/1.1/users/lookup.json?screen_name='+screenNames).done(function(data) {
+			console.log(data);
+			self.get('twittAccountInfos').pushObjects(data);
+		}).fail(function(err) {
+			console.warn('Could not get Data from Twitter',err);
+		});
 	},
 
 
@@ -19,6 +38,20 @@ export default Ember.Controller.extend({
 
 		logout: function() {
 			this.get('twitter').logout();
+		},
+
+		checkAccounts: function() {
+			console.log(this.get('twittAccounts'));
+			var i = this.get('twittAccounts').replace(/(\r\n|\n|\r|,)/gm,"");
+
+			var accounts = i.split('@');
+			if(accounts.length > 0) {
+				accounts.shift();
+				console.log(accounts);
+				this.getAccountInfos(accounts);
+			} else {
+				console.warn('Input Data not parsable');
+			}
 		},
 
 		initService: function() {
