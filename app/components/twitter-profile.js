675ef2a4ac752fb50256600832f752f8b6bb37c1
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-09-13 13:07:36 +0200

    tried to make style bindings more secure

--- app/components/twitter-profile.js
+++ app/components/twitter-profile.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+	twitterProfileImage: Ember.computed('profile.profile_image_url_https', function() {
+		var bg = 'url(' + this.get('profile.profile_image_url_https') + ')';
+		return new Ember.Handlebars.SafeString('background-image: ' + bg);
+	}),
+
 
 	actions: {
 		add: function() {
